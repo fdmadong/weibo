11aafc2c025aeb35af518372f71bb9d3a0e8add3
Breeze <do@qci.cn>
2021-11-07 08:13:06 +0000

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
 
-Route::get('/signup', 'UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
-Route::get('/signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+Route::get('password/reset', 'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'PasswordController@reset')->name('password.update');
+
