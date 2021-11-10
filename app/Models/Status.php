98cf6d98212fad011d207cc7ea87636bf8628182
Breeze <do@qci.cn>
2021-11-10 03:35:27 +0000

    首页微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -11,7 +11,7 @@ class Status extends Model
 
 //    protected $table = 'statuses';
     protected $fillable = ['content'];
-    
+
     public function user()
     {
         return $this->belongsTo(User::class);
